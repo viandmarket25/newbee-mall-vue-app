06b6fc7540e40769d063a0b904660e23736a95ac
winner <viandmarket@outlook.com>
2023-08-15 18:13:41 +0800

    m

--- src/components/home-banner.vue
+++ src/components/home-banner.vue
@@ -4,10 +4,18 @@
       <button @click="prevSlide" class="nav-button prev-button">&#8249;</button>
       <div class="carousel-track" :style="{ transform: `translateX(${translateValue}px)` }">
         <div v-for="(image, index) in images" :key="index" class="carousel-slide">
-          <img :src="image" alt="Carousel Image" class="carousel-image" style="width:100%; height:450px; margin-top:12px; border-radius: 6px; float:left; background-repeat: no-repeat;" />
+          <img :src="image" alt="Carousel Image" class="carousel-image"  style="width:100%; height:450px; margin-top:12px; border-radius: 6px; float:left; background-repeat: no-repeat;" />
         </div>
       </div>
       <button @click="nextSlide" class="nav-button next-button">&#8250;</button>
+    </div>
+    <div class="indicator-container">
+      <span
+        v-for="(image, index) in images"
+        :key="index"
+        :class="{ indicator: true, active: index === currentIndex }"
+        @click="goToSlide(index)"
+      ></span>
     </div>
   </div>
 </template>
@@ -28,6 +36,7 @@ export default {
              'https://static.zara.net/photos///2023/V/0/1/p/2157/065/800/2/w/2062/2157065800_9_1_1.jpg?ts=1682076558860'
            ],
       currentIndex: 0,
+      autoScrollInterval: null,
     };
   },
   mounted() {
@@ -50,7 +59,7 @@ export default {
       }
     },
     startAutoScroll() {
-      this.autoScrollInterval = setInterval(this.autoScroll, 5000);
+      this.autoScrollInterval = setInterval(this.autoScroll, 3000);
     },
     stopAutoScroll() {
       clearInterval(this.autoScrollInterval);
@@ -63,6 +72,10 @@ export default {
         this.currentIndex = 0;
         this.translateValue = 0;
       }
+    },
+    goToSlide(index) {
+      this.currentIndex = index;
+      this.translateValue = -index * this.slideWidth;
     },
   },
 };
@@ -126,5 +139,28 @@ export default {
   padding: 4px;
   background: #f1f1f1;
   right: 10px;
+}
+
+.indicator-container {
+  display: flex;
+  justify-content: center;
+  margin-top: -8px;
+  z-index:99999;
+}
+
+.indicator {
+  width: 18px;
+  height: 3px;
+  background: #ccc;
+  border-radius: 1px;
+
+  margin: 0 2px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.indicator.active {
+    box-shadow:0 0 0 0.6px #666;
+  background: #fff;
 }
 </style>
