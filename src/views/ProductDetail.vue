62ce1339556abdfa4011020fe145dd6b045f8a35
winner <viandmarket@outlook.com>
2023-08-25 01:25:56 +0800

    m

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -10,7 +10,7 @@
 
 <template>
   <div class="product-detail">
-    <s-header style="font-size:14px;" :name="product.attributes.title"></s-header>
+    <s-header v-if="product.attributes" style="font-size:14px;" :name="product.attributes.title"></s-header>
 
 
    
@@ -21,12 +21,17 @@
       <div class="centered-position" style="width:100vw;">
 
       <div class="product-detail-yop">
-        <div class="product-images" style="width:400px;  ">
-          <img  
-            :src="selectedPhoto"    
-            alt="Product Image"
-           
-          />
+        <div class="product-images" style="width:360px;  ">
+
+
+          <transition name="slide-up" mode="out-in">
+            <img
+            class="product-image-tr"
+              :src="selectedPhoto"
+              alt="Product Image"
+            />
+          </transition>
+          <!--
           <img
             v-for="(image, index) in product.media"
             :key="index"
@@ -35,43 +40,176 @@
             alt="Product Image"
             @click="viewImage(image.url)"
           />
+          -->
         </div>
-        <div class="product-thumbnails" style="width:32px; ">
+        <div class="product-thumbnails" style="width:32px; padding-left:10px; border-left:0.4px solid #f1f1f1; ">
           <img
             v-for="(thumbnail, index) in product.media"
             :key="index"
             :src="thumbnail.url"
-            style="width:30px;"
+            style="width:30px; margin-bottom: 0.4px; "
             alt="Product Thumbnail"
             @click="viewImage(thumbnail.url)"
           />
         </div>
-        <div class="product-info left-target-position" style="width:180px; padding:20px; border:0.4px solid #404040; text-align: left;">
-          <p>{{ product.attributes.title }}</p>
-          <p>{{ product.attributes.description }}</p>
-          <p>₦ {{ product.attributes.price }} </p>
-          <p> {{ product.attributes.stockCapacity }} in stock</p>
-
-          <hr/>
-            <div 
-
-          v-for="(variant, index) in product.variants"
-                :key="index" style="margin-bottom: 14px;"
+        <div class="product-info left-target-position" style="width:290px; padding:0px; border:0.4px solid #404040; text-align: left;">
           
-          class="variant-selector">
-            <label>{{ variant.title }}</label>
-            <div class="variant-options">
-              <div
-                v-for="(option, opindex) in variant.options"
-                :key="opindex"
-                :class="`selected-variant `+{ 'selected-variant': variant.selectedOption === opindex }"
-                @click="selectVariant(index, opindex)"
-                style="font-size:10px;"
-              >
-                {{ option.title }}
+          <div style=" padding:20px; " >
+          
+          <p>{{ product.attributes.title }}</p>
+          
+          <p>₦ {{ product.attributes.price }} </p>
+          <p>{{ product.attributes.description }}</p>
+          <p style="color:#404040;"> {{ product.attributes.stockCapacity }} in stock</p>
+
+
+          </div>
+
+
+          <div style=" padding:20px; " >
+            <div>
+              <div class="quantity-container">
+                <button @click="decreaseQuantity" style="border-radius:0px; border:none;" class="quantity-button">-</button>
+                <input type="number" v-model="quantity" class="quantity-input" />
+                <button @click="increaseQuantity" style="border-radius:0px; border:none;" class="quantity-button">+</button>
               </div>
             </div>
+
           </div>
+
+
+          <div v-if="product.variants" style="background-color: #000; height:0.4px;"></div>
+           
+          <div v-if="product.variants"  style=" padding:20px; " >
+          
+          <div 
+              v-for="(variant, index) in product.variants"
+
+              :v-if="variant.variationType=='image-text'"
+
+                    :key="index" style="margin-bottom: 20px;"    
+                class="variant-selector">
+                <label >{{ variant.title }}</label>
+                <div style="margin-bottom: 14px; font-size:12px; " class="variant-options">
+                 
+                  <div
+                  :v-model="variant.selectedOption"
+
+                  :v-if="variant.variationType=='image-text'"
+
+                    v-for="(option, opindex) in variant.options"
+                    :key="opindex"
+                    :class=" variant.selectedOption === opindex?`selected-variant-image`: `variant-option `"
+                    @click="selectVariant(index, opindex)"
+                    style="font-size:10px;"
+                  >
+                  <img
+                  
+                    :src="option.mediaSource"
+                    style="width:60px; max-height:60px; margin-bottom: 0.4px; "
+                    alt="Product Thumbnail"
+                    @click="viewImage(option.mediaSource)"
+                  /><br/>
+                    {{ option.title }}
+                  </div>
+
+
+                </div>
+            </div>
+
+
+
+          <div 
+              v-for="(variant, index) in product.variants"
+
+                  :v-if="variant.variationType=='text'"
+                    :key="index" style="margin-bottom: 20px;"    
+                class="variant-selector">
+                <label >{{ variant.title }}</label>
+                <div style="margin-bottom: 14px; font-size:12px; " class="variant-options">
+                 
+                  <div
+                  :v-model="variant.selectedOption"
+                  :v-if="variant.variationType=='text'"
+                    v-for="(option, opindex) in variant.options"
+                    :key="opindex"
+                    :class=" variant.selectedOption === opindex?`selected-variant`: `variant-option `"
+                    @click="selectVariant(index, opindex)"
+                    style="font-size:10px;"
+                  >
+                    {{ option.title }}
+                  </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+          </div>
+
+
+           <div style="background-color: #000; height:0.4px;"></div>
+
+
+          <div style=" padding:20px; " >
+            <div>
+                        
+              <div class="collapsible">
+                <p @click="toggleShipping" class="header-text-de" style=" cursor:pointer;  font-size:11px; ">SHIPPING</p>
+                <transition name="slide-fade" mode="out-in">
+                  <div v-if="showShipping">
+                    <p>We provide fast and reliable shipping options to ensure your orders are delivered to you promptly and securely.</p>
+                    <p>📦 Standard Shipping: Your order will be delivered within 5-7 business days.</p>
+                    <p>🚀 Express Shipping: If you need your items urgently, choose our express shipping option for delivery within 1-2 business days.</p>
+                    <p>🌎 International Shipping: We offer worldwide shipping. Delivery times may vary depending on your location.</p>
+                    <p>📮 Track Your Order: You can easily track your order's progress through your account.</p>
+                  </div>
+                </transition>
+              </div>
+              
+              <div class="collapsible">
+                <p @click="toggleReturns"  class="header-text-de"  style=" cursor:pointer;  font-size:11px; ">RETURNS</p>
+                <transition name="slide-fade" mode="out-in">
+                  <div v-if="showReturns">
+                    <p>We want you to be completely satisfied with your purchase. If you're not happy with your order, you can return the items within 30 days of receipt.</p>
+                    <p>🔁 Easy Returns Process: Log in to your account, initiate a return request, and follow the instructions provided.</p>
+                    <p>💰 Refund Policy: Once we receive your returned items, we'll process your refund within 5-7 business days.</p>
+                    <p>🔄 Exchanges: If you'd like to exchange an item for a different size or color, please contact our customer support team.</p>
+                  </div>
+                </transition>
+              </div>
+              
+              <div class="collapsible">
+                <p @click="toggleDelivery"  class="header-text-de"  style=" cursor:pointer; font-size:11px; ">DELIVERY</p>
+                <transition name="slide-fade" mode="out-in">
+                  <div v-if="showDelivery">
+                    <p>Our dedicated delivery team ensures that your orders are carefully packaged and promptly delivered to your preferred address.</p>
+                    <p>⏰ Delivery Time: Estimated delivery times are provided during the checkout process based on your location and chosen shipping method.</p>
+                    <p>📦 Safe and Secure Packaging: We take extra care to package your items securely to prevent any damage during transit.</p>
+                    <p>📧 Delivery Confirmation: You'll receive a confirmation email and tracking information once your order has been dispatched.</p>
+                    <p>💼 International Delivery: Please note that international orders may be subject to customs duties and taxes, which are the responsibility of the recipient.</p>
+                  </div>
+                </transition>
+              </div>
+            </div>
+
+          </div>
+
+          
+
+
+
+
+
+
+
+          
+
         
         </div>
 
@@ -84,7 +222,7 @@
 
 
 
-
+    <div style="height:200px;"></div>
       <div class="product-intro">
         <ul>
           <li>Overview</li>
@@ -100,8 +238,8 @@
     <van-goods-action>
       <van-goods-action-icon icon="chat-o" text="Chat" />
       <van-goods-action-icon icon="cart-o" :info="!count ? '' : count" @click="goTo()" text="Cart"/>
-      <input type="button" style="width:44%; border-radius:4px; margin-right:10px; height:40px; color:#404040; background:#fff; border:1px solid #000; " @click="addCart" value="Add to Cart" />
-      <input type="button" style="width:44%; border-radius:4px; height:40px; color:#fff; background:#404040; border:1px solid #000; " @click="goToCart" value="Buy Now" />
+      <input type="button" style="width:44%; border-radius:0px; margin-right:10px; height:60px; color:#404040; background:#fff; border:1px solid #000; " @click="addCart" value="Add to Cart" />
+      <input type="button" style="width:44%; border-radius:0px; height:60px; color:#fff; background:#000; border:1px solid #000; " @click="goToCart" value="Buy Now" />
      
     </van-goods-action>
   </div>
@@ -118,6 +256,10 @@ import productService from '@/service/products'
 export default {
   data() {
     return {
+      quantity: 1,// Initial quantity value
+      showShipping: false,
+      showReturns: false,
+      showDelivery: false,
       selectedPhoto:'',
       product:{},
       variants: [], // Populate with variant options
@@ -138,13 +280,35 @@ export default {
     await this.getProductDetails();
   },
   methods: {
+    increaseQuantity() {
+      this.quantity++;
+    },
+    decreaseQuantity() {
+      if (this.quantity > 1) {
+        this.quantity--;
+      }
+    },
+    toggleShipping() {
+      this.showShipping = !this.showShipping;
+    },
+    toggleReturns() {
+      this.showReturns = !this.showReturns;
+    },
+    toggleDelivery() {
+      this.showDelivery = !this.showDelivery;
+    },
     viewImage(url) {
       // Implement image viewer logic
       this.selectedPhoto=url;
     },
-    selectVariant(variant,option) {
-      alert(option)
-      this.product.variants[variant].selectedOption = option;
+    selectVariant(variantIndex,option) {
+
+      this.product.variants[variantIndex].selectedOption = option;
+
+     // alert("opt"+option)
+     //  alert("vopt"+this.product.variants[variantIndex].selectedOption)
+
+
     },
     async getProductDetails(){
       let id= this.$route.params.id;
@@ -303,23 +467,56 @@ export default {
 .variant-options {
   display: flex;
   gap: 4px;
+
+  flex-wrap: wrap; /* Allow flex items to wrap onto multiple lines */
+   /* Add some spacing between flex items */
 }
 
-.variant-options div {
+
+.variant-option {
   padding: 4px;
+  padding-left:20px;
+  padding-right:20px;
+
  
   border: 1px solid #ccc;
   border-radius: 0px;
   cursor: pointer;
 }
+.variant-option:hover {
+
+  border: 1px solid #404040;
+  border-radius: 0px;
+  cursor: pointer;
+}
+
+.selected-variant-image {
+  padding: 4px;
+  padding-left:20px;
+  padding-right:20px;
+ 
+  border: 1.6px solid #404040;
+  color:#000;
+  border-radius: 0px;
+ cursor: pointer;
+}
+
 
 .selected-variant {
-  border: 2px solid #000;
+  padding: 4px;
+  padding-left:20px;
+  padding-right:20px;
+
+  
+  border: 1.6px solid #404040;
+  color:#000;
+ border-radius: 0px;
+ cursor: pointer;
 }
 
 button {
   padding: 10px 20px;
-  background-color: #007bff;
+  background-color: #000;
   color: #fff;
   border: none;
   border-radius: 4px;
@@ -339,7 +536,7 @@ button {
   float:left;
   flex: 1; /* Equal width columns */
   display: flex;
-  min-height:90vh;
+  min-height:40vh;
   margin:4px;
   flex-direction: column;
    /* Center content vertically */
@@ -350,12 +547,62 @@ button {
   flex: 0 0 40px; /* Fixed width of 40px */
 }
 
+.product-image-tr {
+  /* Initial position for the transition */
+
+}
+
+.collapsible {
+  margin-bottom: 20px;
+}
+
+h2 {
+  cursor: pointer;
+}
+
+.slide-fade-enter-active, .slide-fade-leave-active {
+  transition: all 0.2s ease;
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
 
 
+.header-text-de{
 
 
+}
+
+.header-text-de:hover{
+
+  color:#000;
 
 
+}
 
+.quantity-container {
+  display: flex;
+  align-items: center;
+}
 
+.quantity-button {
+  border:none;
+  outline:none;
+  width:30px;
+ 
+  padding: 2px 6px;
+  font-size: 12px;
+
+  cursor: pointer;
+}
+
+.quantity-input {
+  margin:4px;
+
+  padding: 4px 6px;
+  border: 1px solid #ccc;
+  width: 40px;
+  text-align: center;
+}
 </style>
